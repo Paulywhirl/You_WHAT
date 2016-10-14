be2da59faaf0c14562c02afb9726dfce04bb2734
Paul Henderson <phender9@uwo.ca>
2016-10-14 15:57:46 -0400

    Added some menu stuff

--- app/src/main/java/com/example/phenderson/you_what/MainActivity.java
+++ app/src/main/java/com/example/phenderson/you_what/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.phenderson.you_what;
 
+import android.app.DialogFragment;
 import android.media.MediaPlayer;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -30,4 +36,30 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id ==R.id.action){
+
+            DialogFragment myFragment = new MyDialogFragment();
+
+            myFragment.show(getFragmentManager(), "The Dialog");
+            return true;
+        }
+        else if (id == R.id.close){
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
